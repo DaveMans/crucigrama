aa078f91211a9fff2ad80b7941c6654cc49b741d
DaveMans <c3poy2k@gmail.com>
2016-10-25 22:00:02 -0600

    added all js files

---
+++ js/jquery.countdown.js
@@ -0,0 +1,124 @@
+$(document).ready(function() {
+
+    (function($){
+    
+        $.extend({
+            
+            APP : {                
+                
+                formatTimer : function(a) {
+                    if (a < 10) {
+                        a = '0' + a;
+                    }                              
+                    return a;
+                },    
+                
+                startTimer : function(dir) {
+                    
+                    var a;
+                    
+                    // save type
+                    $.APP.dir = dir;
+                    
+                    // get current date
+                    $.APP.d1 = new Date();
+                    
+                     // get current timestamp (for calculations)
+					$.APP.t1 = $.APP.d1.getTime(); 
+					
+					// if countdown add ms based on seconds in textfield, sets 5min as default
+					$.APP.t1 += parseInt(50)*1000;                         
+                    
+                    // start loop
+                    $.APP.loopTimer();
+                    
+                },
+                
+                loopTimer : function() {
+                    
+                    var td;
+                    var d2,t2;
+                    
+                    var ms = 0;
+                    var s  = 0;
+                    var m  = 0;
+                    var h  = 0;
+                    
+					// get current date and convert it into 
+					// timestamp for calculations
+					d2 = new Date();
+					t2 = d2.getTime();   
+					
+					// calculate time difference between
+					// initial and current timestamp
+					td = $.APP.t1 - t2;
+
+					// calculate milliseconds
+					ms = td%1000;
+					if (ms < 1) {
+						ms = 0;
+					} else {    
+						// calculate seconds
+						s = (td-ms)/1000;
+						if (s < 1) {
+							s = 0;
+						} else {
+							// calculate minutes   
+							var m = (s-(s%60))/60;
+							if (m < 1) {
+								m = 0;
+							} else {
+								// calculate hours
+								var h = (m-(m%60))/60;
+								if (h < 1) {
+									h = 0;
+								}                             
+							}    
+						}
+					}
+				  
+					// substract elapsed minutes & hours
+					ms = Math.round(ms/100);
+					s  = s-(m*60);
+					m  = m-(h*60);                                
+					
+					// update display
+					$('#' + $.APP.dir + '_ms').html($.APP.formatTimer(ms));
+					$('#' + $.APP.dir + '_s').html($.APP.formatTimer(s));
+					$('#' + $.APP.dir + '_m').html($.APP.formatTimer(m));
+										
+					if(m === 0 && s === 30 && ms === 9){
+						//initialize jquery toasty js
+						$("body").toasty();
+						$("body").toasty('pop');						
+					}
+					
+					if(m === 0 && s === 0 && ms === 1){
+						var correctAnswers = $.cookie('correctAnswers');
+						
+						if(correctAnswers !== 'undefined' && correctAnswers < 10){
+							var message = "Te hicieron falta " + (10 - correctAnswers) + " de 10 respuestas correctas.";
+							swal("No lo lograste!!!", message, "error");
+							$("body").find('audio').remove();
+							$("body").append('<audio id="youLoser"><source src="sounds/youLoser.mp3" type="audio/mpeg"></source></audio>');
+							var audio = document.getElementById('youLoser');
+							audio.play();
+						}
+						
+						if(correctAnswers !== 'undefined' && correctAnswers === 10){
+							var message = "Felicitaciones, completaste el crucigrama.";
+							swal("Sos pilas, lo lograste!!!", message, "success");
+							$("body").find('audio').remove();
+							$("body").append('<audio id="wellDone"><source src="sounds/wellDone.mp3" type="audio/mpeg"></source></audio>');
+							var audio = document.getElementById('wellDone');
+							audio.play();
+						}
+					}
+					
+					// loop
+					$.APP.t = setTimeout($.APP.loopTimer,1);
+                }
+            }    
+        });
+    })(jQuery);
+});
---
+++ js/script.js
@@ -0,0 +1,168 @@
+// A javascript-enhanced crossword puzzle [c] Jesse Weisbeck, MIT/GPL 
+(function($) {
+	$(function() {
+		// provide crossword entries in an array of objects like the following example
+		// Position refers to the numerical order of an entry. Each position can have 
+		// two entries: an across entry and a down entry
+		var puzzleData = [
+			 	{
+					clue: "Una declaración moral que elabora afirmaciones y define lo que es bueno, malo, obligatorio, permitido, etc.",
+					answer: "etica",
+					position: 1,
+					orientation: "across",
+					startx: 1,
+					starty: 1,
+					correct: false
+				},
+			 	{
+					clue: "Veneración o el acatamiento que se hace a alguien.",
+					answer: "respeto",
+					position: 3,
+					orientation: "across",
+					startx: 7,
+					starty: 1,
+					correct: false
+				},
+				{
+					clue: "Un sistema formado por elementos naturales y artificiales que están interrelacionados y que son modificados por la acción humana.",
+					answer: "medioambiente",
+					position: 5,
+					orientation: "across",
+					startx: 1,
+					starty: 3,
+					correct: false
+				},
+				{
+					clue: "Refiere a lo que no puede ser dividido. Se trata, por lo tanto, de una unidad independiente (frente a otras unidades) o de una unidad elemental.",
+					answer: "individuo",
+					position: 8,
+					orientation: "across",
+					startx: 1,
+					starty: 5,
+					correct: false
+				},
+				{
+					clue: "Un grupo de individuos marcados por una cultura en común, un cierto folclore y criterios compartidos que condicionan sus costumbres y estilo de vida y que se relacionan entre sí en el marco de una comunidad.",
+					answer: "Sociedad",
+					position: 10,
+					orientation: "across",	
+					startx: 2,
+					starty: 7,
+					correct: false
+				},
+				/*{
+					clue: "Ajustar una estrategia a otra. A nivel de marketing se fijan los objetivos de una empresa en base a unas determinadas circunstancias de producto, mercado, comunicación y gestión comercial",
+					answer: "adaptar",
+					position: 13,
+					orientation: "across",
+					startx: 1,
+					starty: 9
+				},
+				{
+					clue: "Conjunto de creencias, costumbres, valores y normas de una persona o de un grupo social, que funciona como una guía para obrar.",
+					answer: "moral",
+					position: 16,
+					orientation: "across",
+					startx: 1,
+					starty: 11
+				},
+				{
+					clue: "Acción y efecto de unir o unirse (juntar, combinar, atar o acercar dos o más cosas para hacer un todo, ya sea físico o simbólico)",
+					answer: "union",
+					position: 17,
+					orientation: "across",
+					startx: 7,
+					starty: 11
+				},*/
+				{
+					clue: "Acción y efecto de unir o unirse (juntar, combinar, atar o acercar dos o más cosas para hacer un todo, ya sea físico o simbólico)",
+					answer: "union",
+					position: 1,
+					orientation: "down",
+					startx: 1,
+					starty: 1,
+					correct: false
+				},
+				{
+					clue: "Son las siglas que definen MarKeTing",
+					answer: "mkt",
+					position: 2,
+					orientation: "down",
+					startx: 5,
+					starty: 1,
+					correct: false
+				},
+				{
+					clue: "Ajustar una estrategia a otra. A nivel de marketing se fijan los objetivos de una empresa en base a unas determinadas circunstancias de producto, mercado, comunicación y gestión comercial",
+					answer: "adaptar",
+					position: 4,
+					orientation: "down",
+					startx: 9,
+					starty: 1,
+					correct: false
+				},
+				{
+					clue: "Greek variable that usually describes rotor positon",
+					answer: "theta",
+					position: 6,
+					orientation: "down",
+					startx: 7,
+					starty: 3,
+					correct: false
+				},
+				{
+					clue: "Conjunto de creencias, costumbres, valores y normas de una persona o de un grupo social, que funciona como una guía para obrar.",
+					answer: "moral",
+					position: 7,
+					orientation: "down",
+					startx: 12,
+					starty: 4,
+					correct: false
+				},
+				/*{
+					clue: "No. 13 across does this to a voltage",
+					answer: "steps",
+					position: 9,
+					orientation: "down",
+					startx: 5,
+					starty: 5
+				},
+				{
+					clue: "Emits a lout wailing sound",
+					answer: "siren",
+					position: 11,
+					orientation: "down",
+					startx: 11,
+					starty: 7
+				},
+				{
+					clue: "Information technology (abbr.)",
+					answer: "it",
+					position: 12,
+					orientation: "down",
+					startx: 1,
+					starty: 8
+				},
+				{
+					clue: "Es la traducion de prueba al ingles",
+					answer: "test",
+					position: 14,
+					orientation: "down",
+					startx: 3,
+					starty: 9
+				},
+				{
+					clue: "Offset current control (abbr.)",
+					answer: "occ",
+					position: 15,
+					orientation: "down",
+					startx: 7,
+					starty: 9
+				}*/
+			] 
+	
+		$('#puzzle-wrapper').crossword(puzzleData);
+		
+	})
+	
+})(jQuery)
